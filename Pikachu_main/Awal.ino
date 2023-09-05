9fa275df823416c01fda289c731c647c397c9d0a
Muhammad Ahsan Fillah Abadi <106282732+ahsanfi@users.noreply.github.com>
2023-09-06 05:44:11 +0700

    Add files via upload

---
+++ Pikachu_main/Awal.ino
@@ -0,0 +1,110 @@
+void Awal() {
+  
+  // Diplay Animation
+  // Frame1
+// Awal1
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal1, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal2
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal2, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal3
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal3, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal4
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal4, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal5
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal5, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal6
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal6, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal7
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal7, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal8
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal8, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal9
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal9, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal10
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal10, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal11
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal11, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal12
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal12, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal13
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal13, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal14
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal14, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal15
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal15, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal16
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal16, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+// Awal17
+display.clearDisplay();
+display.drawBitmap(0, 0, Awal17, 128, 64, 1);
+display.display();
+delay(frame_delay);
+
+if (frame_delay>50) frame_delay=frame_delay-20;
+loop();
+
+}
